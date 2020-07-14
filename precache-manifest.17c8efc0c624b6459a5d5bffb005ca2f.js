6eb0f2cb7e9fed4b928f752072e9dbf46869ab52
omkar Kesarkhane <omkesarkhane@gmail.com>
2020-07-14 14:53:23 +0530

    Updates

---
+++ precache-manifest.17c8efc0c624b6459a5d5bffb005ca2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e34eb86880737267cb6bba4d5e3495a",
+    "url": "/financial-portfolio-tracker-app/index.html"
+  },
+  {
+    "revision": "ec3ccc872594bf7119cb",
+    "url": "/financial-portfolio-tracker-app/static/css/main.860a6bb9.chunk.css"
+  },
+  {
+    "revision": "97044c6d299868f5bc83",
+    "url": "/financial-portfolio-tracker-app/static/js/2.9820a4df.chunk.js"
+  },
+  {
+    "revision": "74d3d877814d33cc198a0839eaac6439",
+    "url": "/financial-portfolio-tracker-app/static/js/2.9820a4df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3ccc872594bf7119cb",
+    "url": "/financial-portfolio-tracker-app/static/js/main.1dbddce9.chunk.js"
+  },
+  {
+    "revision": "125ffbe5ce058a81f77b",
+    "url": "/financial-portfolio-tracker-app/static/js/runtime-main.b10db3ed.js"
+  }
+]);
